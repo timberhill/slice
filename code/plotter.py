16f4778467b3dd79caacbba2a9e7681db5841b73
Max Timberhill <max.lisogorski@gmail.com>
2018-12-06 09:23:28 +0000

    computed example output

--- code/plotter.py
+++ code/plotter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr
 import settings as settings
@@ -16,6 +17,41 @@ def get_colours_gradient(vals, rgblow, rgbhigh, alpha=(0,1)):
     a = [ norm(val, alpha, actual_bounds) for val in vals ]
 
     return [(x, y, z) for x,y,z in zip(r, g, b)], a
+
+
+def is_outlier(points, thresh=3.5):
+    """
+    Returns a boolean array with True if points are outliers and False 
+    otherwise.
+
+    Parameters:
+    -----------
+        points : An numobservations by numdimensions array of observations
+        thresh : The modified z-score to use as a threshold. Observations with
+            a modified z-score (based on the median absolute deviation) greater
+            than this value will be classified as outliers.
+
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
+
+    References:
+    ----------
+        Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+        Handle Outliers", The ASQC Basic References in Quality Control:
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor. 
+    """
+    if len(points.shape) == 1:
+        points = points[:,None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+	
 
 
 def plot_all_lines():
@@ -44,10 +80,15 @@ def plot_all_lines():
                 ylabel = labels[i][j][1]
 
                 # compute PCCs
-                p, pp = pearsonr(data[xlabel], data[ylabel])
+                x = data[xlabel][~is_outlier(data[xlabel])]
+                x = data[xlabel][~is_outlier(data[ylabel])]
+                y = data[ylabel][~is_outlier(data[xlabel])]
+                y = data[ylabel][~is_outlier(data[ylabel])]
+                
+                p, pp = pearsonr(x, y)
 
                 # plot
-                ax[i,j].scatter(data[xlabel], data[ylabel], 
+                ax[i,j].scatter(x, y, 
                     color='#000b29', marker='+', rasterized=True)
                 ax[i,j].set_xlabel(xlabel)
                 ax[i,j].set_ylabel(ylabel)
--- code/settings.py
+++ code/settings.py
@@ -3,11 +3,11 @@
 lines_list_path = "../data/active_lines.dat"
 
 list_path = "../data/rvdata.dat" # list of files containing columns "filename", "rv"[in km/s] and "logS"
-s1d_folder = "../data/s1d/" # folder which contains *s1d*.fits files from HARPS DRS
-# s1d_folder = "/media/timberhill/TIMBERSTICK/s1d/"
+# s1d_folder = "../data/s1d/" # folder which contains *s1d*.fits files from HARPS DRS
+s1d_folder = "/run/media/timberhill/TIMBERSTICK/s1d/"
 output_path = "../data/results/"
 plots_path = "../data/plots/"
 
 template_n = 10 # number of spectra to stack for low activity template
 
-ncores = 4 # number of cores to utilize
+ncores = 8 # number of cores to utilize
